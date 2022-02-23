c2578d48f9afb210a4194a367a738ac89d5718c5
Pothula Kalyani <Pothula.Kalyani@maplelabs.com>
2022-02-23 14:53:03 +0530

    employee application

--- practiceData/sample_orm_sq.py
+++ practiceData/sample_orm_sq.py
@@ -15,9 +15,9 @@ class employee_details(Model):
       gender=IntegerField(foreign_key=True,default=3)
       contact=IntegerField()
       age=IntegerField()'''
-
-    email = StringField()
     name = StringField()
+    email = StringField()
+    
     gender = IntegerField()
     contact = IntegerField()
     age = IntegerField()
@@ -25,25 +25,56 @@ class employee_details(Model):
 
 class gender(Model):
     id = IntegerField(primary_key=True)
-    genderstr = StringField()
+    gender = StringField()
 
 
 db = Database(file_path)
 print(db)
 employee_details.objects.backend = db
 gender.objects.backend = db
-
+employee_id=int(input("enter employee id :"))
+actionto=input("enter action to be performed : ")
 employee = employee_details.objects.all()
 print(db)
+def read_emp_data(employee_id):
+        
+   print(employee_details.objects.get(pk=employee_id))
+def update_emp_data(employee_id):
+    emp=employee_details.objects.get(pk=employee_id)
+    emp["email"]=input("enter email :")
+    emp["contact"]=int(input("enter contact :"))
+    emp["name"]=input("enter name :")
+    emp.update()
+    print(emp)    
+def delete_emp_data(employee_id):
+    emp=employee_details.objects.get(pk=employee_id).delete()
+    print(emp)
 
-
+def insert_emp_data(employee_id):
+   emp= employee_details({'id':employee_id,'email':input("enter email :"),'name':input("enter name :"),'gender':input("enter gender: "),'contact':input("enter contact:"),"age":input("enter age: ")})
+   employee_details.objects.add(emp)
+   employee_details.objects.save()
+   print(emp)
+def action(actionto): 
+        if(actionto=='read'): 
+                        read_emp_data(employee_id)
+        elif(actionto=="update"):
+                        update_emp_data(employee_id)
+        elif(actionto=="insert"):
+                        insert_emp_data(employee_id)
+        elif(actionto=="delete"):
+                        delete_emp_data(employee_id)
+        else: print(employee_details.objects.all()) 
+print(employee_details.objects.all())
+action(actionto)  
+employee_details.objects.backend.close()
 @app.route('/employees', methods=['Get'])
+
+
 def table():
     response = jsonify(employee)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+     
 
-
-# print(employee_details.objects.get(pk=103))
-# print(gender.objects.get(pk=1))
 app.run()
